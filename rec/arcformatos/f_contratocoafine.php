8521d79038aff38e4899c4fb62091c609fa937b2
cristinalg <cristina_13logan@hotmail.com>
2019-02-25 10:30:05 -0600

    f_contratocoafine.php

--- rec/arcformatos/f_contratocoafine.php
+++ rec/arcformatos/f_contratocoafine.php
@@ -68,7 +68,7 @@ if($val5=mysqli_fetch_array($res2))
 
 
 	
-	//$hoy="$lugar a $dia de ".mes($mes)." de $anio";
+	$hoy="$lugar a $dia de ".mes($mes)." de $anio";
 	}
 mysqli_free_result($res2);
 
@@ -139,7 +139,7 @@ if($val5=mysql_fetch_array($res2))
 mysql_free_result($res2);
 
 */
-	### GeneraciÃ³n de PDF
+	### Generación de PDF
 
 $pdf=new PDF_WriteTag();
 
@@ -183,7 +183,7 @@ $pdf->Cell(10,$es,"A.",0,0,"C");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(3);
 
-$txt="<p>Que su representante cuenta con los poderes suficientes para representar y obligar a la sociedad en este acto, tal y como consta en la Escritura Publica Número 69502 de fecha 5 de marzo de 2013, pasada ante la fe del Licenciado Luis De Angoitia Becerra, Notario Público número 230 del Distrito Federal, <vb>(anexo 3)</vb>, los cuales bajo protesta de decir verdad manifiesta que no le han sido revocados ni modificados de forma alguna.</p>";
+$txt="<p>Que su representante cuenta con los poderes suficientes para representar y obligar a la sociedad en este acto, tal y como consta en la Escritura Pública Número 69502 de fecha 5 de marzo de 2013, pasada ante la fe del Licenciado Luis De Angoitia Becerra, Notario Público número 230 del Distrito Federal, <vb>(anexo 3)</vb>, los cuales bajo protesta de decir verdad manifiesta que no le han sido revocados ni modificados de forma alguna.</p>";
 $pdf->Cell(10,$es,"B.",0,0,"C");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(3);
@@ -208,16 +208,17 @@ $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(3);
 
 $pdf->SetFont('Arial','',$fs);
-$txt="<p>Que es una persona con plena facultad física y mental para celebrar este contrato el cual tiene como objeto recibir el crédito autorizado por el Instituto  del Fondo nacional para la vivienda de los trabajadores,  de acuerdo y para la incorporación al programa de apoyo de línea cuatro.  otorgado con la finalidad de que este contrate el servicio de ejecución de la obra con la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> consistente en mantenimiento, remodelación de vivienda, o en su caso el de Supervisión de obra, dado que el derechohabiente tiene la libre   voluntad de elegir a su conveniencia quien ejecutará los trabajos de remodelación de su casa habitación. Por lo que, por así convenir a su interés de elegir a otra constructora para la ejecución de la obra consistente en la remodelación, contratara en este caso (LA CONSTRUCTORA) solo como la encargada de la supervisión de los trabajos de remodelación de vivienda.</p>";
+$txt="<p>Que es una persona con plena facultad física y mental para celebrar este contrato el cual tiene como objeto recibir el crédito autorizado por el Instituto  del Fondo Nacional para la Vivienda de los Trabajadores,  de acuerdo y para la incorporación al programa de apoyo de línea cuatro.  otorgado con la finalidad de que este contrate el servicio de ejecución de la obra con la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> consistente en mantenimiento, remodelación de vivienda, o en su caso el de Supervisión de obra, dado que el derechohabiente tiene la libre   voluntad de elegir a su conveniencia quien ejecutará los trabajos de remodelación de su casa habitación. Por lo que, por así convenir a su interés de elegir a otra constructora para la ejecución de la obra consistente en la remodelación, contratara en este caso (LA CONSTRUCTORA) solo como la encargada de la supervisión de los trabajos de remodelación de vivienda.</p>";
 $pdf->Cell(10,$es,"A.",0,0,"C");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(3);
 
-$txt="<p>Que su domicilio para los efectos del presente contrato se encuentra ubicado en: <placee>ARGENTINOS 61, MARIA G DE GARCIA RUIZ, ALVARO OBREGON, CIUDAD DE MEXICO, C.P. 01160.</placee><br>                        </p>";
+$txt="<p>Que su domicilio para los efectos del presente contrato se encuentra ubicado en: <placee>".$domicilio.".</placee></p>";
 $pdf->Cell(10,$es,"B.",0,0,"C");
 $pdf->WriteTag(0,$es,$txt,0,"J");
-$pdf->Ln(0);
+$pdf->Ln(3);
 
+$pdf->SetFont('Arial','',$fs);
 $txt="<p>Que es su voluntad celebrar este contrato con la “CONSTRUCTORA” para la ejecución de lo trabajos de remodelación de vivienda objeto del préstamo adquirido o en su caso de acuerdo a su libre voluntad y de acuerdo a lo establecido en las clausulas “D” y “E” punto 2 el contrato maestro celebrado con el “ADMINISTRADOR” de los fondos, encomendar los trabajos motivo del crédito recibido a la constructora de su elección de acuerdo a sus intereses.</p>";
 $pdf->Cell(10,$es,"C.",0,0,"C");
 $pdf->WriteTag(0,$es,$txt,0,"J");
@@ -242,11 +243,11 @@ $pdf->SetFont('Arial','BU',$fs);
 $pdf->Cell(180,$es,"C  L  Á  U  S  U  L  A  S",0,1,"C");
 $pdf->Ln(2);
 
-$txt="<p><vb>PRIMERA.</vb> <p3>Objeto.</p3><vb>-</vb> “EL DERECHOHABIENTE” por voluntad propia decide llevar a cabo la remodelación de su casa habitación y acepta las condiciones contractuales que el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRA BAJADORES ha llevado a cabo con el Fideicomiso Promotoría Asociados esta es tu Casa, S.A. de C.V., y ésta a su vez contra los servicios de la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb>  Para la ejecución de la obra, mediante crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES al derechohabiente, derivado del programa de apoyo de línea cuatro, por lo que, y de acuerdo a su libre voluntad de selección del ejecutor de la obra el derechohabiente decida contratar a otra Constructora para la encomienda de la ejecución de la misma, por lo que de ser así   <vb>VITTA COCINAS DE ESTILO S.A. DE C. V.</vb>   Entregará el importe del crédito  otorgado, al derechohabiente, dado que los fondos provienen de crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES.</p>";
+$txt="<p><vb>PRIMERA.</vb> <p3>Objeto.</p3><vb>-</vb> “EL DERECHOHABIENTE” por voluntad propia decide llevar a cabo la remodelación de su casa habitación y acepta las condiciones contractuales que el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES ha llevado a cabo con el Fideicomiso Promotoría Asociados esta es tu Casa, S.A. de C.V., y ésta a su vez contra los servicios de la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb>  Para la ejecución de la obra, mediante crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES al derechohabiente, derivado del programa de apoyo de línea cuatro, por lo que, y de acuerdo a su libre voluntad de selección del ejecutor de la obra el derechohabiente decida contratar a otra Constructora para la encomienda de la ejecución de la misma, por lo que de ser así   <vb>VITTA COCINAS DE ESTILO S.A. DE C. V.</vb>   Entregará el importe del crédito  otorgado, al derechohabiente, dado que los fondos provienen de crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>SEGUNDA.- </vb>En caso de que el derechohabiente  acreditado decidiera sustituir a la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A.DE C.V.</vb> mediante  convenio y una vez que se acuerde la devolucion entrega del credito otorgado al derechohabiente el mismo  se compromete a contratar a esta única y exclusivamente con la finalidad de que se lleven a cabo la supervisión de los trabajos de remodelación objeto del préstamo otorgado y de acuerdo  al programa de apoyo de línea cuatro, por lo que además se compromete   a pagar a la “CONSTRUCTORA ” un __% del importe del préstamo adquirido por concepto de supervisión de los trabajos a realizar, ya sea remodelación, mantenimiento o ampliación de vivienda.</p>";
+$txt="<p><vb>SEGUNDA.- </vb>En caso de que el derechohabiente  acreditado decidiera sustituir a la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A.DE C.V.</vb> mediante  convenio y una vez que se acuerde la devolución entrega del crédito otorgado al derechohabiente el mismo  se compromete a contratar a esta única y exclusivamente con la finalidad de que se lleven a cabo la supervisión de los trabajos de remodelación objeto del préstamo otorgado y de acuerdo  al programa de apoyo de línea cuatro, por lo que además se compromete   a pagar a la “CONSTRUCTORA” un __% del importe del préstamo adquirido por concepto de supervisión de los trabajos a realizar, ya sea remodelación, mantenimiento o ampliación de vivienda.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
@@ -254,15 +255,15 @@ $txt="<p><vb>TERCERA.</vb> <p3>Vigencia.</p3><vb>-</vb> La vigencia del presente
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>CUARTA.- </vb>En caso de que el “EL DERECHOHABIENTE” decidiera sustituir a la “CONSTRUCTORA” de acuerdo a su libre voluntad de selección de que constructora elija para la ejecución de los trabajos, mediante convenio este recibirá por parte de la “LA CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb>    el importe de $ _____________________M.N., mediante cheque nominativo proveniente del crédito otorgado por parte del INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES, para que de acuerdo a su libre voluntad  pueda contratar a la “CONSTRUCTORA” única y exclusivamente para la supervisión de los trabajos que ejecute la constructora elegida por el derechohabiente, mediante el pago de un porcentaje establecido sobre el crédito recibido por el acreditado para que éste a su vez, continúe con el trámite contractual de un tercero (Constructora) para la ejecución de la obra consistente en los trabajos encomendados.</p>";
+$txt="<p><vb>CUARTA.- </vb>En caso de que el “EL DERECHOHABIENTE” decidiera sustituir a la “CONSTRUCTORA” de acuerdo a su libre voluntad de selección de que constructora elija para la ejecución de los trabajos, mediante convenio este recibirá por parte de la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb>    el importe de $ _____________________M.N., mediante cheque nominativo proveniente del crédito otorgado por parte del INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES, para que de acuerdo a su libre voluntad  pueda contratar a la “CONSTRUCTORA”  única y exclusivamente para la supervisión de los trabajos que ejecute la constructora elegida por el derechohabiente, mediante el pago de un porcentaje establecido sobre el crédito recibido por el acreditado para que éste a su vez, continúe con el trámite contractual de un tercero (Constructora) para la ejecución de la obra consistente en los trabajos encomendados.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>QUINTA.-</vb> <p3>Entrega del credito.</p3><vb>-</vb> En el caso de que  “EL DERECHOHABIENTE”  decidiera sustituir  a la “CONSTRUCTORA”  <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> para la ejecución de  los trabajos de remodelación, acudirá a las oficinas de  esta  para la entrega del crédito otorgado, fondos que provienen del INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES, mediante  cheque nominativo que será otorgado por el encargado de cuentas por pagar  el día que acuerden ambas partes y firmará una copia que será archivado en el expediente para control, y queda a su libre voluntad de contratar a la  “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> para la supervisión de la obra mediante el pago por el servicio prestado.</p>";
+$txt="<p><vb>QUINTA.-</vb> <p3>Entrega del crédito.</p3><vb>-</vb> En el caso de que  “EL DERECHOHABIENTE”  decidiera sustituir  a la “CONSTRUCTORA”  <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> para la ejecución de  los trabajos de remodelación, acudirá a las oficinas de  esta  para la entrega del crédito otorgado, fondos que provienen del INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES, mediante  cheque nominativo que será otorgado por el encargado de cuentas por pagar  el día que acuerden ambas partes y firmará una copia que será archivado en el expediente para control, y queda a su libre voluntad de contratar a la  “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> para la supervisión de la obra mediante el pago por el servicio prestado.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>SEXTA.</vb> <p3>Impuestos.</p3><vb>-</vb> En el  acto  de que la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> entregue al derechohabiente el importe del préstamo otorgado, derivado de la sustitución del ejecutor de obra y de acuerdo a su libre voluntad de elegir a un tercero para la ejecución de lo trabajos de remodelación no se genera ningún tipo de impuesto por considerarse un préstamo para remodelación, ampliación o construcción de casa habitación fondos que provienen  por parte del Instituto Nacional para la Vivienda de los Trabajadores  para la incorporación al programa de apoyo de línea cuatro conforme a la Legislaciónn Fiscal vigente, obligándose el derechohabiente a pagar el crédito otorgado a la Instituto.</p>";
+$txt="<p><vb>SEXTA.</vb> <p3>Impuestos.</p3><vb>-</vb> En el  acto  de que la “CONSTRUCTORA” <vb>VITTA COCINAS DE ESTILO S.A. DE C.V.</vb> entregue al derechohabiente el importe del préstamo otorgado, derivado de la sustitución del ejecutor de obra y de acuerdo a su libre voluntad de elegir a un tercero para la ejecución de lo trabajos de remodelación no se genera ningún tipo de impuesto por considerarse un préstamo para remodelación, ampliación o construcción de casa habitación fondos que provienen  por parte del Instituto Nacional para la Vivienda de los Trabajadores  para la incorporación al programa de apoyo de línea cuatro conforme a la Legislación Fiscal vigente, obligándose el derechohabiente a pagar el crédito otorgado a la Instituto.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
@@ -270,11 +271,11 @@ $txt="<p><vb>SEPTIMA.</vb> <p3>Naturaleza de este contrato.</p3><vb>-</vb> Toman
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>OCTAVA.</vb> <p3>Celebración de diversos contratos.</p3><vb>-</vb> “EL DERECHOHABIENTE”  podrá celebrar con cualquier otra persona llámese persona física o persona moral todo tipo de contratos de prestación de servicios de remodelación a fin de cumplir con el propósito del crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES el entendido de que el “DERECHOHABIENTE” Será el único responsable de pagarle al tercero el importe de los materiales y todo lo que implique para encomendada remodelación.</p>";
+$txt="<p><vb>OCTAVA.</vb> <p3>Celebración de diversos contratos.</p3><vb>-</vb> “EL DERECHOHABIENTE”  podrá celebrar con cualquier otra persona llámese persona física o persona moral todo tipo de contratos de prestación de servicios de remodelación a fin de cumplir con el propósito del crédito otorgado por el INSTITUTO DEL FONDO NACIONAL DE LA VIVIENDA PARA LOS TRABAJADORES el entendido de que el“DERECHOHABIENTE” Será el único responsable de pagarle al tercero el importe de los materiales y todo lo que implique para encomendada remodelación.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
-$txt="<p><vb>L</vb>os contratantes convienen en que para la interpretación y cumplimiento de este contrato, se someten en forma expresa a las leyes y tribunales de la Ciudad de México, renunciando desde luego a cualquier  fuero que pudiera corresponderles por domicilios presentes o futuros, leído y ratificado en todas y cada una de sus partes el presente contrato y enteradas de todas y cada una de las clausulas contenidas en este instrumento y manifestando su conformidad con las mismas, lo firman para constancia de aceptación y conocimiento de su alcance y valor legal en la Ciudad de México a los …. de Octubre de 2018.</p>";
+$txt="<p><vb>L</vb>os contratantes convienen en que para la interpretación y cumplimiento de este contrato, se someten en forma expresa a las leyes y tribunales de la Ciudad de México, renunciando desde luego a cualquier  fuero que pudiera corresponderles por domicilios presentes o futuros, leído y ratificado en todas y cada una de sus partes el presente contrato y enteradas de todas y cada una de las clausulas contenidas en este instrumento y manifestando su conformidad con las mismas, lo firman para constancia de aceptación y conocimiento de su alcance y valor legal en la Ciudad de México $hoy.</p>";
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(2);
 
@@ -310,27 +311,27 @@ $pdf->Cell(50,$es,"……………………………………",0,1,"C");
 $pdf->Ln(2);
 
 /*
-$txt="<p>Tengo plena conciencia de que es un crÃ©dito y que si no realizo la mejoras no se utilizara el SSV para liquidarlo y seguirÃ© pagando el crÃ©dito hasta su liquidaciÃ³n total.</p>";
+$txt="<p>Tengo plena conciencia de que es un crÃƒÂ©dito y que si no realizo la mejoras no se utilizara el SSV para liquidarlo y seguirÃƒÂ© pagando el crÃƒÂ©dito hasta su liquidaciÃƒÂ³n total.</p>";
 $pdf->Cell(12,$es,"A)",0,0,"R");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(1);
 
-$txt="<p>Se que tienen 180 dÃ­as para hacer las remodelaciones correspondientes y que no existe prÃ³rroga para ampliar el plazo.</p>";
+$txt="<p>Se que tienen 180 dÃƒÂ­as para hacer las remodelaciones correspondientes y que no existe prÃƒÂ³rroga para ampliar el plazo.</p>";
 $pdf->Cell(12,$es,"B)",0,0,"R");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(1);
 
-$txt="<p>Se que tengo que cumplir al pie de la letra el programa y presupuesto de obra ya que el hecho de no hacerlo pone en riesgo que el dictamen de aplicaciÃ³n de recursos (segunda visita) resulte negativo por ende no se liquidara mi crÃ©dito.</p>";
+$txt="<p>Se que tengo que cumplir al pie de la letra el programa y presupuesto de obra ya que el hecho de no hacerlo pone en riesgo que el dictamen de aplicaciÃƒÂ³n de recursos (segunda visita) resulte negativo por ende no se liquidara mi crÃƒÂ©dito.</p>";
 $pdf->Cell(12,$es,"C)",0,0,"R");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(1);
 
-$txt="<p>Que una vez concluidas las mejoras se realizara una segunda visita a la vivienda bajo el objeto del crÃ©dito y de proceder de emitirÃ¡ un DAR positivo donde se determinara que se jale el SSV, para liquidar el crÃ©dito.</p>";
+$txt="<p>Que una vez concluidas las mejoras se realizara una segunda visita a la vivienda bajo el objeto del crÃƒÂ©dito y de proceder de emitirÃƒÂ¡ un DAR positivo donde se determinara que se jale el SSV, para liquidar el crÃƒÂ©dito.</p>";
 $pdf->Cell(12,$es,"D)",0,0,"R");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 $pdf->Ln(1);
 
-$txt="<p>Que una vez emitido el DAR positivo el tiempo estimado para que el crÃ©dito se vea liquidado y por ende poder bajar el aviso de suspensiÃ³n de descuentos es de 3 meses contados a partir de que el Ã¡rea tÃ©cnica envÃ­a el dictamen a oficinas centrales.</p>";
+$txt="<p>Que una vez emitido el DAR positivo el tiempo estimado para que el crÃƒÂ©dito se vea liquidado y por ende poder bajar el aviso de suspensiÃƒÂ³n de descuentos es de 3 meses contados a partir de que el ÃƒÂ¡rea tÃƒÂ©cnica envÃƒÂ­a el dictamen a oficinas centrales.</p>";
 $pdf->Cell(12,$es,"E)",0,0,"R");
 $pdf->WriteTag(0,$es,$txt,0,"J");
 
